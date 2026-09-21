ff9a24c5909ba401ad89ad6e891b1bad9aec8f2f
unknown <chanderp@cybermedia.co.in>
2026-09-21 15:36:02 +0530

    Update custom.css: change font-size for elementor button text

--- functions.php
+++ functions.php
@@ -6866,5 +6866,16 @@ add_shortcode( 'brand_report_form', 'cmg_render_brand_report_form' );
 add_shortcode( 'brand_analysis_report', 'cmg_render_brand_report_form' );
 add_shortcode( 'cmg_report_form', 'cmg_render_brand_report_form' );
 
+function my_custom_styles() {
+    wp_enqueue_style(
+        'custom-css',
+        get_template_directory_uri() . '/css/custom.css',
+        array(),
+        time()
+    );
+}
+add_action('wp_enqueue_scripts', 'my_custom_styles', 999);
+
+
 
 
